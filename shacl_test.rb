eb505b8131f71d943f838ffb2e94441f3e340ddb
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-08-27 12:33:54 -0400

    covering required and disambiguation properties

--- shacl_test.rb
+++ shacl_test.rb
@@ -4,19 +4,19 @@ require 'shacl'
 require 'rdf/turtle'
 
 class ShaclTest < Minitest::Test
-  def setup
-   
-  end
 
-  def test_sameas_facebook_event
-    shacl = SHACL.open("shacl/check_facebook_link.ttl")
+  def test_event_start_date
+    shacl = SHACL.open("shacl/event_start_date.ttl")
 
-    graph = RDF::Graph.load("fixtures/sameas_facebook_event_bad.jsonld")
-    report =  shacl.execute(graph) 
-    assert !report.conform?,"#{report}"
-
-    graph = RDF::Graph.load("fixtures/sameas_facebook_event_good.jsonld")
+    # bad
+    graph = RDF::Graph.load("fixtures/event_start_date_bad.jsonld")
     report =  shacl.execute(graph)  
+    assert_equal 2,report.results.length
+
+    # good
+    graph = RDF::Graph.load("fixtures/event_start_date_good.jsonld")
+    report =  shacl.execute(graph)  
+    puts report
     assert report.conform?,"#{report}"
   end
 
@@ -35,6 +35,50 @@ class ShaclTest < Minitest::Test
     puts report
     assert report.conform?,"#{report}"
   end
+
+  def test_place
+    shacl = SHACL.open("shacl/place.ttl")
+
+    # bad
+    graph = RDF::Graph.load("fixtures/place_bad.jsonld")
+    report =  shacl.execute(graph)  
+    assert_equal 2,report.results.length
+
+    # good
+    graph = RDF::Graph.load("fixtures/place_good.jsonld")
+    report =  shacl.execute(graph)  
+    assert report.conform?,"#{report}"
+  end
+
+  def test_postal_address_postal_code
+    shacl = SHACL.open("shacl/postal_address_postal_code.ttl")
+
+    # bad
+    graph = RDF::Graph.load("fixtures/postal_address_postal_code_bad.jsonld")
+    report =  shacl.execute(graph)  
+    puts report
+    assert_equal 2,report.results.length
+
+    # good
+    graph = RDF::Graph.load("fixtures/postal_address_postal_code_good.jsonld")
+    report =  shacl.execute(graph)  
+    puts report
+    assert report.conform?,"#{report}"
+  end
+
+  def test_sameas_facebook_event
+    shacl = SHACL.open("shacl/check_facebook_link.ttl")
+
+    graph = RDF::Graph.load("fixtures/sameas_facebook_event_bad.jsonld")
+    report =  shacl.execute(graph) 
+    assert !report.conform?,"#{report}"
+
+    graph = RDF::Graph.load("fixtures/sameas_facebook_event_good.jsonld")
+    report =  shacl.execute(graph)  
+    assert report.conform?,"#{report}"
+  end
+
+
 
 
 end
--- sparql_test.rb
+++ sparql_test.rb
@@ -16,13 +16,13 @@ class SparqlTest < Minitest::Test
   def test_required_properties
     # If any of the three required properties is missing, the score would be 0 (zero), no matter how good the rest of the structured data is.
     # An event with all three required properties would have a score of 12 + (2 x 8) = 28.
-    graph = RDF::Graph.load("fixtures/required_properties.jsonld")
+    graph = RDF::Graph.load("fixtures/score_tests.jsonld")
     graph <<  @shacl.execute(graph) 
     graph.query(@sparql)
     puts graph.dump(:ttl)
-    actual = graph.query([RDF::URI('http://example.org/2'), RDF::URI('http://example.org/scoreTotal'), nil]).first.object.value.to_i
+    actual = graph.query([RDF::URI('http://example.org/2'), RDF::URI('http://example.org/score'), nil]).first.object.value.to_i
     assert_equal 28, actual
-    actual = graph.query([RDF::URI('http://example.org/1'), RDF::URI('http://example.org/scoreTotal'), nil]).first.object.value.to_i
+    actual = graph.query([RDF::URI('http://example.org/1'), RDF::URI('http://example.org/score'), nil]).first.object.value.to_i
     assert_equal 0, actual
 
   end
