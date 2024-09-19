d4081f2b8ab08e9a6491cfb8c773c760ef82e187
dev <dev.aravind@culturecreates.com>
2024-09-19 16:43:43 +0530

    Added support to generate CSV reports for score

---
+++ src/main.rb
@@ -0,0 +1,34 @@
+require 'rdf'
+require 'sparql'
+require 'shacl'
+require 'linkeddata'
+require 'csv'
+require 'fileutils'
+
+url = ARGV[0]
+
+filename = url.split('/').last.split('.').first
+graph = RDF::Graph.load(url)
+
+SHACL.open('shacl/shacl_for_scoring.ttl').execute(graph)
+
+SPARQL.execute(File.read('sparql/score_algorithm.sparql'), graph, update: true)
+report = SPARQL.execute(File.read('sparql/report.sparql'), graph)
+
+report.to_csv
+
+if report.any?
+  FileUtils.mkdir_p('reports')
+  CSV.open("reports/#{filename}_report.csv", "w") do |csv|
+    headers = report.first.to_h.keys
+    csv << headers
+
+    report.each do |solution|
+      csv << headers.map { |header| solution[header].to_s }
+    end
+  end
+
+  puts "CSV report generated: #{filename}_report.csv"
+else
+  puts "No results to generate CSV."
+end
